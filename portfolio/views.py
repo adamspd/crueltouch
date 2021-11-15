633b1d4741ccf28b08f08d41cc34a784a0177054
Adams P D <57180807+adamspd@users.noreply.github.com>
2021-11-15 16:06:50 +0100

    Added the possibility to show randomly the photos from the albums in the portfolio page thus will allow to add multiple images for one album in a row without worring about the fact that all the photos that looks the same or are in the same album will be together (and doesn't look very good)  - I transformed a the queryset album into a list and then used random.shuffle to shuffle the list and passed that to the view

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,7 +1,11 @@
+import random
+
 from django.shortcuts import render
 from portfolio.models import Photo
 
 
 def index_portfolio(request):
     all_photos = Photo.objects.all()
-    return render(request, 'portfolio/index_portfolio.html', {'all_photos': all_photos})
+    list_of_photo = list(all_photos)
+    random.shuffle(list_of_photo)
+    return render(request, 'portfolio/index_portfolio.html', {'all_photos': list_of_photo})
