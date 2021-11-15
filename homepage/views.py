6b5c2ded412d6268473c7c50d605a959b2fd6f82
Adams P D <57180807+adamspd@users.noreply.github.com>
2021-11-15 18:06:32 +0100

    Photo on the frontpage will be shown randomly at each reload

--- homepage/views.py
+++ homepage/views.py
@@ -12,11 +12,15 @@ from .models import Photo
 def index(request):
     album1 = Photo.objects.filter(album_id=1)
     album2 = Photo.objects.filter(album_id=2)
+    list_of_album1 = list(album1)
+    list_of_album2 = list(album2)
+    random.shuffle(list_of_album1)
+    random.shuffle(list_of_album2)
     # random.shuffle(album1, random=None)
     # random.shuffle(album2, random=None)
     context = {
-        'album1': album1,
-        'album2': album2
+        'album1': list_of_album1,
+        'album2': list_of_album2
     }
     return render(request, 'homepage/index.html', context)
 
