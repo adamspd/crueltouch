f9ae826ae30fce89452765c11fb7d8ab670f50ec
Adams P D <57180807+adamspd@users.noreply.github.com>
2021-11-18 21:02:41 +0100

    Users can click on a photo to have a better view on the portfolio page

--- portfolio/views.py
+++ portfolio/views.py
@@ -9,3 +9,8 @@ def index_portfolio(request):
     list_of_photo = list(all_photos)
     random.shuffle(list_of_photo)
     return render(request, 'portfolio/index_portfolio.html', {'all_photos': list_of_photo})
+
+
+def detailed_view(request, pk):
+    photo = Photo.objects.get(id=pk)
+    return render(request, 'homepage/detailed.html', {'photo': photo})
