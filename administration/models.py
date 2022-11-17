57e56d035a71fba5b8a6e2d1733ccd8981f80fe7
Adams Pierre David <57180807+adamspd@users.noreply.github.com>
2022-11-17 20:02:56 +0100

    Aesthetics of button, verbose name in permission class updated

--- administration/models.py
+++ administration/models.py
@@ -1,17 +1,18 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 # Create your models here.
 
 class PermissionsEmails(models.Model):
-    date = models.CharField(max_length=255, null=False, blank=False, unique=True)
-    number_of_emails = models.IntegerField(default=0)
+    date = models.CharField(max_length=255, null=False, blank=False, unique=True, verbose_name=_('The date'))
+    number_of_emails = models.IntegerField(default=0, verbose_name=_('Number of emails sent for booking request'))
 
     def __str__(self):
         return str(self.date)
 
     class Meta:
-        verbose_name_plural = "Permissions Emails"
+        verbose_name_plural = _('Permissions Emails')
 
     @property
     def can_send_email(self):
