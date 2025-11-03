eb5b06b7b2dd957c969e38b081fcbe4587934428
Adams Pierre David <57180807+adamspd@users.noreply.github.com>
2025-11-03 20:17:08 +0100

    remove creation of thumbnails on every request since it's done upon saving in the model

--- portfolio/views.py
+++ portfolio/views.py
@@ -7,7 +7,6 @@ from portfolio.models import Photo, Album
 def index_portfolio(request):
     all_photos = Photo.objects.all()
     # list of 2 photos from all photos
-    check_format_photo(all_photos)
     all_albums = Album.objects.all()
     list_of_photo = list(all_photos)
     random.shuffle(list_of_photo)
@@ -15,9 +14,3 @@ def index_portfolio(request):
         'all_photos': list_of_photo,
         'all_albums': all_albums,
     })
-
-
-def check_format_photo(list_of_photo: list[Photo]):
-    for photo in list_of_photo:
-        photo.change_file_format()
-        photo.create_thumbnail()
